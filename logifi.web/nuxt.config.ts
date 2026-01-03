d9b2f8aff7f3c859deafd621858c92f398bfe280
derek <dfarm1999@gmail.com>
2026-01-03 09:43:20 -0500

    favicon.png added.  Needs reworked

--- logifi.web/nuxt.config.ts
+++ logifi.web/nuxt.config.ts
@@ -29,13 +29,14 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      
       link: [
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon.png' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600;700&display=swap' }
-          ]
-
+      ]
     }
   }
 })
